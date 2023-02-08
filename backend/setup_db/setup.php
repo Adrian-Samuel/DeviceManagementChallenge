7fb1ea444e04e1bff377916b2f34026e67c30bf1
Adrian Samuel <adrian.samuel@machinemax.com>
2023-02-08 05:46:20 +0000

    cleaned file ordering, fixed bash script commands

--- backend/setup_db/setup.php
+++ backend/setup_db/setup.php
@@ -1,10 +1,13 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/seed.sql';
+
 use Dotenv\Dotenv;
 
-require __DIR__ . '/vendor/autoload.php';
+
 const APP_ROOT = __DIR__;
-$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv::createImmutable( APP_ROOT . '/..');
 $dotenv->load();
 
 
@@ -21,10 +24,10 @@ try {
     $conn = new PDO("pgsql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = file_get_contents("seed.sql");
+    $sql = file_get_contents(__DIR__ . '/seed.sql');
     $conn->exec($sql);
 
-    echo "Database seeded successfully.";
+    echo "Database seeded successfully.\n";
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
--- backend/startup.sh
+++ backend/startup.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-bash "${PWD}/setup_db/setup.php" && php -S localhost:8000 "${PWD}/index.php"
+php "${PWD}/setup_db/setup.php" &&
+echo "Server starting..." &&
+php -S localhost:8000 "${PWD}/index.php"
